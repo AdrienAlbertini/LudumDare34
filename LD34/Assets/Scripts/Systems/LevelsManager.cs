f5079b11e93cd49667807530c80c6694379f4345
Adrien Albertini <a.albertini2b@gmail.com>
2015-12-12 15:56:38 +0100

    [ADD] assets folder; [UPD] Parallax; [ADD] Resize sprite about screen; [FIX] parallax

---
+++ LD34/Assets/Scripts/Systems/LevelsManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class LevelsManager : MonoBehaviour
+{
+    public List<string> scenes = new List<string>();
+    private int _currentSceneId = 0;
+    private bool _isNextSceneLoaded = false;
+    private AsyncOperation _asyncOp;
+
+    // Use this for initialization
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    IEnumerator Preloader()
+    {
+        this._asyncOp.allowSceneActivation = false;
+        this._asyncOp = SceneManager.LoadSceneAsync(this.scenes[_currentSceneId + 1], LoadSceneMode.Additive);
+        yield return this._asyncOp;
+    }
+
+    public void PreloadNextScene()
+    {
+        StartCoroutine(this.Preloader());
+    }
+
+    public void LoadNextScene()
+    {
+        this._asyncOp.allowSceneActivation = true;
+    }
+
+    public void LoadScene(int sceneId)
+    {
+        if (sceneId >= 0 && sceneId < this.scenes.Count)
+        {
+            SceneManager.LoadScene(sceneId, LoadSceneMode.Single);
+        }
+    }
+
+    public void LoadScene(string scene)
+    {
+        if (this.scenes.Contains(scene))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
---
+++ LD34/Assets/Scripts/Systems/Parallax/Parallax_BGScroller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax_BGScroller : MonoBehaviour
+{
+    public float scrollSpeed = 1.0f;
+    public bool isVerticalScroll = false;
+
+    private float _tileSize = 0.0f;
+    private Vector3 _startPosition = Vector3.zero;
+    private SpriteRenderer _renderer;
+
+    void Start()
+    {
+        GameObject child = new GameObject();
+        SpriteRenderer childSPR = child.AddComponent<SpriteRenderer>();
+        Vector3 tmp = this.transform.position;
+        this._startPosition = transform.position;
+        this._renderer = this.GetComponent<SpriteRenderer>();
+
+
+        // Set tile size
+        if (this.isVerticalScroll)
+            this._tileSize = this._renderer.sprite.bounds.size.y * this.transform.localScale.y;
+        else
+            this._tileSize = this._renderer.sprite.bounds.size.x * this.transform.localScale.x;
+
+        Debug.Log("TileSize: " + this._tileSize + " | Aspect: " + Camera.main.aspect);
+        // set child
+        childSPR.sprite = this._renderer.sprite;
+        child.transform.localScale = this.transform.localScale;
+
+        if (this.isVerticalScroll)
+            tmp.y += this._renderer.sprite.bounds.size.y * this.transform.localScale.y;
+        else
+            tmp.x -= this._renderer.sprite.bounds.size.x * this.transform.localScale.x;
+        child.transform.position = tmp;
+        child.transform.SetParent(this.transform);
+    }
+
+    void FixedUpdate()
+    {
+        float newPos = Mathf.Repeat(Time.time * this.scrollSpeed, this._tileSize);
+
+        if (this.isVerticalScroll)
+            transform.position = this._startPosition + (Vector3.down * newPos);
+        else
+            transform.position = this._startPosition + (Vector3.right * newPos);
+    }
+}
