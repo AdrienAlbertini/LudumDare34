3d3d3df6c37809f1f8d929376327e4a0ac1791be
edmondj <julien.edmond@epitech.eu>
2015-12-12 16:09:20 +0100

    ADD - PlayerSpawner

---
+++ LD34/Assets/Scripts/Systems/PlayerSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject PlayerPrefab;
+
+    void Awake()
+    {
+        if (this.PlayerPrefab == null)
+            Debug.LogError("No player prefab selected");
+        else
+        {
+            Instantiate(this.PlayerPrefab, this.transform.position, Quaternion.identity);
+        }
+    }
+}
