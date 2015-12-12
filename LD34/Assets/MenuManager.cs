234e5a18a704716576f6857690538894ab4dee79
NGambini <noe.gambini@gmail.com>
2015-12-12 16:53:58 +0100

    Menu prefab (to be put in every scene)

---
+++ LD34/Assets/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject holder;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void toMainMenu()
+    {
+        LevelsManager.Instance.LoadScene("MainMenu");
+    }
+
+    public void Resume()
+    {
+        this.holder.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Press Enter To Start Game");
+        if (Event.current.Equals(Event.KeyboardEvent("Escape")))
+        {
+            this.holder.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+}
