9fbeb4894298acca7161ce78d95f86d48c2c29a5
edmondj <julien.edmond@epitech.eu>
2015-12-13 00:02:51 +0100

    Fixed fading completely

--- LD34/Assets/Scripts/Systems/SceneFader.cs
+++ LD34/Assets/Scripts/Systems/SceneFader.cs
@@ -11,7 +11,7 @@ public class SceneFader : SingletonBehaviour<SceneFader>
     [SerializeField]
     public float fadeSpeed = 1.5f;
 
-    private bool _fadeToClear = true;
+    private bool? _fadeToClear = null;
 
     private Image _fadeImg;
 
@@ -24,18 +24,25 @@ public class SceneFader : SingletonBehaviour<SceneFader>
 
     void FixedUpdate()
     {
-        this._fadeImg.color = Color.Lerp(this._fadeImg.color, (this._fadeToClear ? Color.clear : Color.black), this.fadeSpeed * Time.fixedDeltaTime);
-        
-        if (this._fadeToClear && this._fadeImg.color.a <= 0.05f)
+        if (this._fadeToClear != null)
         {
-            this._fadeImg.color = Color.clear;
-            this._fadeImg.enabled = false;
-            if (this.fadeOver != null)
-                this.fadeOver(this, EventArgs.Empty);
-        }
-        else if (!this._fadeToClear && this._fadeImg.color.a >= 0.95f)
-        {
-            this._fadeImg.color = Color.black;
+            this._fadeImg.color = Color.Lerp(this._fadeImg.color, (this._fadeToClear.Value ? Color.clear : Color.black), this.fadeSpeed * Time.fixedDeltaTime);
+
+            if (this._fadeToClear.Value && this._fadeImg.color.a <= 0.05f)
+            {
+                this._fadeImg.color = Color.clear;
+                if (this.fadeOver != null)
+                    this.fadeOver(this, EventArgs.Empty);
+                this._fadeToClear = null;
+                this._fadeImg.enabled = false;
+            }
+            else if (!this._fadeToClear.Value && this._fadeImg.color.a >= 0.95f)
+            {
+                this._fadeImg.color = Color.black;
+                if (this.fadeOver != null)
+                    this.fadeOver(this, EventArgs.Empty);
+                this._fadeToClear = null;
+            }
         }
     }
 
