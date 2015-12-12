cbc926ec1df51ca489e129c4140c72d89d90ed9e
Deretsun <rivapaulantoine@gmail.com>
2015-12-12 18:41:00 +0100

    [UPD] Lvl1 + LvlVertical [ADD] Saving state

---
+++ LD34/Assets/Scripts/Systems/SaveData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class saveData
+{
+    public int levelID;
+}
