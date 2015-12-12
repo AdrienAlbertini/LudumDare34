569c003e2fac82bb39b5efba90fb63ec76425b00
Adrien Albertini <a.albertini2b@gmail.com>
2015-12-12 11:42:50 +0100

    CLEAN + Add base project

---
+++ LD34/Assets/Scripts/Systems/ParallaxBG.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBG : MonoBehaviour
+{
+    public float scrollSpeed = 1.0f;
+    public float tileSizeZ = 0.0f;
+
+    private Vector3 _startPosition = Vector3.zero;
+
+    void Start()
+    {
+        this._startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * this.scrollSpeed, this.tileSizeZ);
+        transform.position = this._startPosition + Vector3.forward * newPos;
+    }
+}
