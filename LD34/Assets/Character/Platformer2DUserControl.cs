36c4126a0d852e95abc5b0ff7fdb0f2e2220e206
edmondj <julien.edmond@epitech.eu>
2015-12-13 14:50:48 +0100

    Fixed jump issues

--- LD34/Assets/Character/Platformer2DUserControl.cs
+++ LD34/Assets/Character/Platformer2DUserControl.cs
@@ -3,201 +3,198 @@ using UnityEngine;
 using UnityStandardAssets._2D;
 using UnityStandardAssets.CrossPlatformInput;
 
-    [RequireComponent(typeof (PlatformerCharacter2D))]
-    public class Platformer2DUserControl : MonoBehaviour
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class Platformer2DUserControl : MonoBehaviour
+{
+    [SerializeField]
+    bool IsPLayerA = false;
+    [SerializeField]
+    CharacterManager MainController;
+    [SerializeField]
+    Transform[] RightCheck;
+    [SerializeField]
+    Transform[] LefttCheck;
+    [SerializeField]
+    Transform[] BottomtCheck;
+    [SerializeField]
+    Transform[] TopCheck;
+    private PlatformerCharacter2D m_Character;
+    private bool canJump = true;
+
+
+
+    private void Awake()
     {
-        [SerializeField] bool IsPLayerA = false;
-        [SerializeField] CharacterManager MainController;
-        [SerializeField] Transform[] RightCheck;
-        [SerializeField] Transform[] LefttCheck;
-        [SerializeField] Transform[] BottomtCheck;
-        [SerializeField] Transform[] TopCheck;
-        private PlatformerCharacter2D m_Character;
-        private bool m_Jump;
-        private bool canJump = true;
-        
-
-
-        private void Awake()
-        {
-            m_Character = GetComponent<PlatformerCharacter2D>();
-        }
-
-
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
-            
-            if (IsPLayerA)
-            {
-                this.transform.localScale = new Vector3(MainController.SizePlayerA,MainController.SizePlayerA,MainController.SizePlayerA);
-            }
-            else
-            {
-                this.transform.localScale = new Vector3(MainController.SizePLayerB,MainController.SizePLayerB,MainController.SizePLayerB);
-            }
-                        bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = 0.0f;
-            float v = 0.0f;
-            if (IsPLayerA)
-            {
-                 h = CrossPlatformInputManager.GetAxis("Horizontal");
-                 v = CrossPlatformInputManager.GetAxis("Vertical");
-            }
-            else
-            {
-                 h = CrossPlatformInputManager.GetAxis("HorizontalB");
-                 v = CrossPlatformInputManager.GetAxis("VerticalB");
-            }
-            bool Jump = false;
-            bool Grow = false;
-            if (v > 0.3)
-            {
-                Debug.Log("Jumping " + v);
-                Jump = true;
-            }
-            else if (v < -0.3)
-            {
-                Grow = true;
-            }
-            MainController.Grow(Grow, IsPLayerA);
-            // Pass all parameters to the character control script.
-            if (canJump == false)
-                Jump = false;
-            m_Character.Move(h, crouch, Jump);
-            m_Jump = false;
-            Jump = false;
-        }
-
-
-        public bool calculateIfIcanGrow()
-        {
-         bool ret = false;
-         float MinDistanceXRight = 500.0f;
-         float MinDistanceXLeft = 500.0f;
-         float MinDistanceYTop = 500.0f;
-         float MinDistanceYBottom = 500.0F;
-         float MinDistanceX = 0.0f;
-         float MinDistanceY = 0.0f;
-         float Size = 0.0f;
-         
-         
-            if (IsPLayerA)
-            {
-                Size = MainController.SizePlayerA;
-            }
-            else
-            {
-                Size = MainController.SizePLayerB;
-            }
-         
-         
-         foreach (Transform tra in RightCheck)
-            {
-                 RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.right), 3.0f);
-                 for (int i = 0; i < collidersLeftTop.Length; i++)
-                 {
-                         if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
-                        {
-                            if (MinDistanceXRight > collidersLeftTop[i].distance)
-                                MinDistanceXRight = collidersLeftTop[i].distance;
-                         }
-                     }
-             }
-            foreach (Transform tra in LefttCheck)
-            {
-                 RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.right * -1), 3.0f);
-                 for (int i = 0; i < collidersLeftTop.Length; i++)
-                 {
-                         if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
-                        {
-                            if (MinDistanceXLeft > collidersLeftTop[i].distance)
-                                MinDistanceXLeft = collidersLeftTop[i].distance;
-                         }
-                     }
-             }
-             
-              foreach (Transform tra in TopCheck)
-            {
-                 RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.up), 3.0f);
-                 for (int i = 0; i < collidersLeftTop.Length; i++)
-                 {
-                         if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
-                        {
-                            if (MinDistanceYTop > collidersLeftTop[i].distance)
-                                MinDistanceYTop = collidersLeftTop[i].distance;
-                         }
-                     }
-             }
-            foreach (Transform tra in BottomtCheck)
-            {
-                 RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.up * -1), 3.0f);
-                 for (int i = 0; i < collidersLeftTop.Length; i++)
-                 {
-                         if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
-                        {
-                            if (MinDistanceYBottom > collidersLeftTop[i].distance)
-                                MinDistanceYBottom = collidersLeftTop[i].distance;
-                         }
-                     }
-             }
-             
-          /*   if (MinDistanceYBottom == 500.0f)
-                MinDistanceYBottom = -1.0f;
-             if (MinDistanceYTop == 500.0f)
-                MinDistanceYTop = -1.0f;
-            */ 
-            /* if (MinDistanceXRight == 500.0f)
-                MinDistanceXRight = -1.0f;
-             if (MinDistanceXLeft == 500.0f)
-                MinDistanceXLeft = -1.0f;*/
-             MinDistanceY = MinDistanceYBottom + MinDistanceYTop;
-             MinDistanceX =  MinDistanceXLeft + MinDistanceXRight;
-             if (MinDistanceX < 0)
-                ret =  true;
-             else if (MinDistanceX == 0)
-                ret =  false;
-             else if (MinDistanceX > 0.1f)
-                ret =  true;
-             else if (MinDistanceX >= 500.0f)
-                ret = true;
-             else
-                ret =  false;
-             if (ret == false)
-             {
-                 m_Character.GetComponent<Rigidbody2D>().gravityScale = 0;
-                 Vector2 vec =  m_Character.GetComponent<Rigidbody2D>().velocity;
-                 vec.y = 0.0f;
-                 m_Character.GetComponent<Rigidbody2D>().velocity = vec;
-                 Debug.Log("Nogravity" + "   " + IsPLayerA);
-                 canJump = false;
-             }
-             else
-             {
-                 m_Character.GetComponent<Rigidbody2D>().gravityScale = 3;
-                 canJump = true;
-             }
-               
-             bool ret2 = false;  
-             if (MinDistanceY < 0)
-                ret2 =  true;
-             else if (MinDistanceY == 0)
-                ret2 =  false;
-             else if (MinDistanceY > 0.1f)
-                ret2 =  true;
-             else if (MinDistanceY >= 500.0f)
-                ret = true;
-             else
-                ret2 =  false;
-             if (ret == true && ret2 == true)
-                return true;
-             else
-                return false;
-       
-               
-            }   
+        m_Character = GetComponent<PlatformerCharacter2D>();
     }
+
+
+    private void Update()
+    {
+        if (IsPLayerA)
+        {
+            this.transform.localScale = new Vector3(MainController.SizePlayerA, MainController.SizePlayerA, MainController.SizePlayerA);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(MainController.SizePLayerB, MainController.SizePLayerB, MainController.SizePLayerB);
+        }
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = 0.0f;
+        float v = 0.0f;
+        if (IsPLayerA)
+        {
+            h = CrossPlatformInputManager.GetAxis("Horizontal");
+            v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            h = CrossPlatformInputManager.GetAxis("HorizontalB");
+            v = CrossPlatformInputManager.GetAxisRaw("VerticalB");
+        }
+        bool Jump = false;
+        bool Grow = false;
+        if (v > 0)
+        {
+            Jump = true;
+        }
+        else if (v < 0)
+        {
+            Grow = true;
+        }
+        MainController.Grow(Grow, IsPLayerA);
+        // Pass all parameters to the character control script.
+        if (canJump == false)
+            Jump = false;
+        m_Character.Move(h, crouch, Jump);
+        Jump = false;
+    }
+
+
+    public bool calculateIfIcanGrow()
+    {
+        bool ret = false;
+        float MinDistanceXRight = 500.0f;
+        float MinDistanceXLeft = 500.0f;
+        float MinDistanceYTop = 500.0f;
+        float MinDistanceYBottom = 500.0F;
+        float MinDistanceX = 0.0f;
+        float MinDistanceY = 0.0f;
+        float Size = 0.0f;
+
+
+        if (IsPLayerA)
+        {
+            Size = MainController.SizePlayerA;
+        }
+        else
+        {
+            Size = MainController.SizePLayerB;
+        }
+
+
+        foreach (Transform tra in RightCheck)
+        {
+            RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.right), 3.0f);
+            for (int i = 0; i < collidersLeftTop.Length; i++)
+            {
+                if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
+                {
+                    if (MinDistanceXRight > collidersLeftTop[i].distance)
+                        MinDistanceXRight = collidersLeftTop[i].distance;
+                }
+            }
+        }
+        foreach (Transform tra in LefttCheck)
+        {
+            RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.right * -1), 3.0f);
+            for (int i = 0; i < collidersLeftTop.Length; i++)
+            {
+                if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
+                {
+                    if (MinDistanceXLeft > collidersLeftTop[i].distance)
+                        MinDistanceXLeft = collidersLeftTop[i].distance;
+                }
+            }
+        }
+
+        foreach (Transform tra in TopCheck)
+        {
+            RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.up), 3.0f);
+            for (int i = 0; i < collidersLeftTop.Length; i++)
+            {
+                if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
+                {
+                    if (MinDistanceYTop > collidersLeftTop[i].distance)
+                        MinDistanceYTop = collidersLeftTop[i].distance;
+                }
+            }
+        }
+        foreach (Transform tra in BottomtCheck)
+        {
+            RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(tra.transform.position, (this.transform.up * -1), 3.0f);
+            for (int i = 0; i < collidersLeftTop.Length; i++)
+            {
+                if (collidersLeftTop[i].transform.gameObject != gameObject && collidersLeftTop[i].transform.gameObject.transform.tag != "Player")
+                {
+                    if (MinDistanceYBottom > collidersLeftTop[i].distance)
+                        MinDistanceYBottom = collidersLeftTop[i].distance;
+                }
+            }
+        }
+
+        /*   if (MinDistanceYBottom == 500.0f)
+              MinDistanceYBottom = -1.0f;
+           if (MinDistanceYTop == 500.0f)
+              MinDistanceYTop = -1.0f;
+          */
+        /* if (MinDistanceXRight == 500.0f)
+            MinDistanceXRight = -1.0f;
+         if (MinDistanceXLeft == 500.0f)
+            MinDistanceXLeft = -1.0f;*/
+        MinDistanceY = MinDistanceYBottom + MinDistanceYTop;
+        MinDistanceX = MinDistanceXLeft + MinDistanceXRight;
+        if (MinDistanceX < 0)
+            ret = true;
+        else if (MinDistanceX == 0)
+            ret = false;
+        else if (MinDistanceX > 0.1f)
+            ret = true;
+        else if (MinDistanceX >= 500.0f)
+            ret = true;
+        else
+            ret = false;
+        if (ret == false)
+        {
+            m_Character.GetComponent<Rigidbody2D>().gravityScale = 0;
+            Vector2 vec = m_Character.GetComponent<Rigidbody2D>().velocity;
+            vec.y = 0.0f;
+            m_Character.GetComponent<Rigidbody2D>().velocity = vec;
+            Debug.Log("Nogravity" + "   " + IsPLayerA);
+            canJump = false;
+        }
+        else
+        {
+            m_Character.GetComponent<Rigidbody2D>().gravityScale = 3;
+            canJump = true;
+        }
+
+        bool ret2 = false;
+        if (MinDistanceY < 0)
+            ret2 = true;
+        else if (MinDistanceY == 0)
+            ret2 = false;
+        else if (MinDistanceY > 0.1f)
+            ret2 = true;
+        else if (MinDistanceY >= 500.0f)
+            ret = true;
+        else
+            ret2 = false;
+        if (ret == true && ret2 == true)
+            return true;
+        else
+            return false;
+
+
+    }
+}
--- LD34/Assets/Character/PlatformerCharacter2D.cs
+++ LD34/Assets/Character/PlatformerCharacter2D.cs
@@ -5,12 +5,19 @@ namespace UnityStandardAssets._2D
 {
     public class PlatformerCharacter2D : MonoBehaviour
     {
-        [SerializeField] private float m_MaxJumpVelocity = 50.0f;  
-        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-        [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
-        [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
-        [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        [SerializeField]
+        private float m_MaxJumpVelocity = 50.0f;
+        [SerializeField]
+        private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+        [SerializeField]
+        private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+        [Range(0, 1)]
+        [SerializeField]
+        private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
+        [SerializeField]
+        private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+        [SerializeField]
+        private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
         const float k_GroundedRadius = .05f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private bool m_LeftCollide;
@@ -18,10 +25,9 @@ namespace UnityStandardAssets._2D
         public Transform[] m_GroundCheck;   // A position marking where to check for ceilings
         private Transform m_TopCheck;   // A position marking where to check for ceilings
         private Transform m_DownCheck;   // A position marking where to check for ceilings
-        private bool JumpstateLastFrame = false;
-        
+
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
-       // private Animator m_Anim;            // Reference to the player's animator component.
+                                            // private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
         private BoxCollider2D m_collider;
@@ -31,7 +37,7 @@ namespace UnityStandardAssets._2D
             // Setting up references.
             m_TopCheck = transform.Find("TopCheck");
             m_DownCheck = transform.Find("BottomCheck");
-         //   m_Anim = GetComponent<Animator>();
+            //   m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
             m_collider = GetComponent<BoxCollider2D>();
         }
@@ -49,50 +55,50 @@ namespace UnityStandardAssets._2D
             GameObject m_PLayertotransfer = null;
             foreach (Transform tra in m_GroundCheck)
             {
-                 RaycastHit2D[] colliders = Physics2D.RaycastAll(tra.position, (this.transform.up * -1), k_GroundedRadius, m_WhatIsGround);
-                 for (int i = 0; i < colliders.Length; i++)
-                 {
-                     if (colliders[i].transform.gameObject != gameObject)
-                     {
-                      m_Grounded = true;
-                      if (colliders[i].transform.gameObject.transform.tag == "Player")
-                      {
-                          m_transferForce = true;
-                          m_PLayertotransfer = colliders[i].transform.gameObject;
-                      }
-                     }
-                 }   
+                RaycastHit2D[] colliders = Physics2D.RaycastAll(tra.position, (this.transform.up * -1), k_GroundedRadius, m_WhatIsGround);
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].transform.gameObject != gameObject)
+                    {
+                        m_Grounded = true;
+                        if (colliders[i].transform.gameObject.transform.tag == "Player")
+                        {
+                            m_transferForce = true;
+                            m_PLayertotransfer = colliders[i].transform.gameObject;
+                        }
+                    }
+                }
             }
-            
+
             if (m_transferForce)
             {
-                Vector2 vel =  m_PLayertotransfer.GetComponent<Rigidbody2D>().velocity;
+                Vector2 vel = m_PLayertotransfer.GetComponent<Rigidbody2D>().velocity;
                 Vector2 _vel = this.m_Rigidbody2D.velocity;
                 _vel.x += vel.x;
                 this.m_Rigidbody2D.velocity = _vel;
             }
-            
+
             RaycastHit2D[] collidersLeftTop = Physics2D.RaycastAll(m_TopCheck.position, (this.transform.right * -1), 0.5f * this.transform.localScale.x + 0.05f, m_WhatIsGround);
             for (int i = 0; i < collidersLeftTop.Length; i++)
             {
                 if (collidersLeftTop[i].transform.gameObject != gameObject)
                     m_LeftCollide = true;
             }
-            
-            RaycastHit2D[] collidersLeftBottom = Physics2D.RaycastAll(m_DownCheck.position, (this.transform.right * -1), 0.5f * this.transform.localScale.x +  0.05f, m_WhatIsGround);
+
+            RaycastHit2D[] collidersLeftBottom = Physics2D.RaycastAll(m_DownCheck.position, (this.transform.right * -1), 0.5f * this.transform.localScale.x + 0.05f, m_WhatIsGround);
             for (int i = 0; i < collidersLeftBottom.Length; i++)
             {
                 if (collidersLeftBottom[i].transform.gameObject != gameObject)
                     m_LeftCollide = true;
             }
-            
-            RaycastHit2D[] collidersRightTop = Physics2D.RaycastAll(m_TopCheck.position, (this.transform.right), 0.5f * this.transform.localScale.x +  0.05f, m_WhatIsGround);
+
+            RaycastHit2D[] collidersRightTop = Physics2D.RaycastAll(m_TopCheck.position, (this.transform.right), 0.5f * this.transform.localScale.x + 0.05f, m_WhatIsGround);
             for (int i = 0; i < collidersRightTop.Length; i++)
             {
                 if (collidersRightTop[i].transform.gameObject != gameObject)
                     m_RightCollide = true;
             }
-            
+
             Debug.DrawRay(m_DownCheck.position, this.transform.right * 0.51f * this.transform.localScale.x, Color.white, 0.1f);
             RaycastHit2D[] collidersRightBottom = Physics2D.RaycastAll(m_DownCheck.position, (this.transform.right), 0.5f * this.transform.localScale.x + 0.05f, m_WhatIsGround);
             for (int i = 0; i < collidersRightBottom.Length; i++)
@@ -102,10 +108,10 @@ namespace UnityStandardAssets._2D
                     m_RightCollide = true;
                 }
             }
-           // m_Anim.SetBool("Ground", m_Grounded);
+            // m_Anim.SetBool("Ground", m_Grounded);
 
             // Set the vertical animation
-           // m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+            // m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
         }
 
 
@@ -120,14 +126,14 @@ namespace UnityStandardAssets._2D
             if (m_Grounded || m_AirControl)
             {
                 // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
+                move = (crouch ? move * m_CrouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
                 ///  m_Anim.SetFloat("Speed", Mathf.Abs(move));
 
 
                 // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+                m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight)
@@ -135,31 +141,31 @@ namespace UnityStandardAssets._2D
                     // ... flip the player.
                     Flip();
                 }
-                    // Otherwise if the input is moving the player left and the player is facing right...
+                // Otherwise if the input is moving the player left and the player is facing right...
                 else if (move < 0 && m_FacingRight)
                 {
                     // ... flip the player.
                     Flip();
                 }
             }
+
             // If the player should jump...
-            if (m_Grounded && jump && JumpstateLastFrame == false)
+            if (m_Grounded && jump)
             {
                 // Add a vertical force to the player.
                 m_Grounded = false;
-              //  m_Anim.SetBool("Ground", false);
-               // m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-              Vector2 tmp =  m_Rigidbody2D.velocity;
-              float velocityY =  m_JumpForce * this.transform.localScale.x;
-              if (velocityY > m_MaxJumpVelocity)
-                velocityY = m_MaxJumpVelocity;
-              tmp.y = velocityY;
-              m_Rigidbody2D.velocity = tmp;
+                //  m_Anim.SetBool("Ground", false);
+                // m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                Vector2 tmp = m_Rigidbody2D.velocity;
+                float velocityY = m_JumpForce * this.transform.localScale.x;
+                if (velocityY > m_MaxJumpVelocity)
+                    velocityY = m_MaxJumpVelocity;
+                tmp.y = velocityY;
+                m_Rigidbody2D.velocity = tmp;
             }
-                        JumpstateLastFrame = jump;
         }
 
-       
+
 
         private void Flip()
         {
