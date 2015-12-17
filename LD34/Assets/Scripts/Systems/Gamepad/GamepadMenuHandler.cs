93e5ef6f8ca0dcc3430278577763137f00c79f21
Adrien Albertini <a.albertini2b@gmail.com>
2015-12-17 01:05:32 +0100

    [UPD] controls img; [UPD] pancartes and lvls

--- LD34/Assets/Scripts/Systems/Gamepad/GamepadMenuHandler.cs
+++ LD34/Assets/Scripts/Systems/Gamepad/GamepadMenuHandler.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class GamepadMenuHandler : MonoBehaviour
 {
     public GamepadButtonHandler currentSelectedButton;
+    public GameObject holder;
     public float untriggerThreshold = 0.4f;
     public float triggerThreshold = 0.6f;
     public Color unselectedBackColor;
@@ -12,7 +13,7 @@ public class GamepadMenuHandler : MonoBehaviour
     public Color unselectedTextColor;
     public Color selectedTextColor;
     private bool _hTriggered, _vTriggered, _pressTriggered;
-    
+
     void Awake()
     {
         this.currentSelectedButton.GetComponent<Image>().color = this.selectedBackColor;
@@ -21,63 +22,66 @@ public class GamepadMenuHandler : MonoBehaviour
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        if (this.holder.activeSelf)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyUp(KeyCode.JoystickButton0))
-            this._pressTriggered = false;
-        if (Input.GetKeyDown(KeyCode.JoystickButton0) && this._pressTriggered == false)
-        {
-            this._pressTriggered = true;
-            AudioManager.Instance.PlaySound("MenuJoystickPush");
-            currentSelectedButton.TriggerButton();
-            if (this.currentSelectedButton.onTriggerNextButton != null)
-                this.NewSelectedButton(this.currentSelectedButton.onTriggerNextButton);
-        }
-        else
-        {
-            if (h >= -untriggerThreshold && h <= untriggerThreshold)
+            if (Input.GetKeyUp(KeyCode.JoystickButton0))
+                this._pressTriggered = false;
+            if (Input.GetKeyDown(KeyCode.JoystickButton0) && this._pressTriggered == false)
             {
-                this._hTriggered = false;
+                this._pressTriggered = true;
+                AudioManager.Instance.PlaySound("MenuJoystickPush");
+                currentSelectedButton.TriggerButton();
+                if (this.currentSelectedButton.onTriggerNextButton != null)
+                    this.NewSelectedButton(this.currentSelectedButton.onTriggerNextButton);
             }
-            if (v >= -untriggerThreshold && v <= untriggerThreshold)
+            else
             {
-                this._vTriggered = false;
-            }
-            if (h >= triggerThreshold)
-            {
-                if (currentSelectedButton.right != null && !this._hTriggered)
+                if (h >= -untriggerThreshold && h <= untriggerThreshold)
                 {
-                    this._hTriggered = true;
-                    this._vTriggered = false;
-                    this.NewSelectedButton(currentSelectedButton.right);
-                }
-            }
-            else if (h <= -triggerThreshold)
-            {
-                if (currentSelectedButton.left != null && !this._hTriggered)
-                {
-                    this._hTriggered = true;
-                    this._vTriggered = false;
-                    this.NewSelectedButton(currentSelectedButton.left);
-                }
-            }
-            if (v >= triggerThreshold)
-            {
-                if (currentSelectedButton.up != null && !this._vTriggered)
-                {
-                    this._vTriggered = true;
                     this._hTriggered = false;
-                    this.NewSelectedButton(currentSelectedButton.up);
                 }
-            }
-            else if (v <= -triggerThreshold)
-            {
-                if (currentSelectedButton.down != null && !this._vTriggered)
+                if (v >= -untriggerThreshold && v <= untriggerThreshold)
                 {
-                    this._vTriggered = true;
-                    this._hTriggered = false;
-                    this.NewSelectedButton(currentSelectedButton.down);
+                    this._vTriggered = false;
+                }
+                if (h >= triggerThreshold)
+                {
+                    if (currentSelectedButton.right != null && !this._hTriggered)
+                    {
+                        this._hTriggered = true;
+                        this._vTriggered = false;
+                        this.NewSelectedButton(currentSelectedButton.right);
+                    }
+                }
+                else if (h <= -triggerThreshold)
+                {
+                    if (currentSelectedButton.left != null && !this._hTriggered)
+                    {
+                        this._hTriggered = true;
+                        this._vTriggered = false;
+                        this.NewSelectedButton(currentSelectedButton.left);
+                    }
+                }
+                if (v >= triggerThreshold)
+                {
+                    if (currentSelectedButton.up != null && !this._vTriggered)
+                    {
+                        this._vTriggered = true;
+                        this._hTriggered = false;
+                        this.NewSelectedButton(currentSelectedButton.up);
+                    }
+                }
+                else if (v <= -triggerThreshold)
+                {
+                    if (currentSelectedButton.down != null && !this._vTriggered)
+                    {
+                        this._vTriggered = true;
+                        this._hTriggered = false;
+                        this.NewSelectedButton(currentSelectedButton.down);
+                    }
                 }
             }
         }
@@ -85,12 +89,15 @@ public class GamepadMenuHandler : MonoBehaviour
 
     public void NewSelectedButton(GamepadButtonHandler newButton)
     {
-        Debug.Log("New Selected Button: " + newButton.gameObject.name);
-        this.currentSelectedButton.GetComponent<Image>().color = this.unselectedBackColor;
-        this.currentSelectedButton.GetComponentInChildren<Text>().color = this.unselectedTextColor;
-        this.currentSelectedButton = newButton;
-        this.currentSelectedButton.GetComponent<Image>().color = this.selectedBackColor;
-        this.currentSelectedButton.GetComponentInChildren<Text>().color = this.selectedTextColor;
-        AudioManager.Instance.PlaySound("MenuJoystickMove");
+        if (this.holder.activeSelf)
+        {
+            Debug.Log("New Selected Button: " + newButton.gameObject.name);
+            this.currentSelectedButton.GetComponent<Image>().color = this.unselectedBackColor;
+            this.currentSelectedButton.GetComponentInChildren<Text>().color = this.unselectedTextColor;
+            this.currentSelectedButton = newButton;
+            this.currentSelectedButton.GetComponent<Image>().color = this.selectedBackColor;
+            this.currentSelectedButton.GetComponentInChildren<Text>().color = this.selectedTextColor;
+            AudioManager.Instance.PlaySound("MenuJoystickMove");
+        }
     }
 }
